81c75394a1f39cf7974d704ac282df7861d78ddf
Aryan Sapra <aryansaprajiit@gmail.com>
2024-05-29 22:16:06 +0530

    sendMessage and addUser requestes are handled

--- src/main/java/com/messaginapp/messageapplication/controllers/ChatController.java
+++ src/main/java/com/messaginapp/messageapplication/controllers/ChatController.java
@@ -3,6 +3,7 @@ package com.messaginapp.messageapplication.controllers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import com.messaginapp.messageapplication.entities.ChatMessage;
@@ -13,6 +14,15 @@ public class ChatController {
     @MessageMapping("/message")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+
+        //add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
 }
